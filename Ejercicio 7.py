b33e0a10b498f5abcac35bec3d8297d6fa9bff6d
Ediberto Coronel <edibertocoronel@gmail.com>
2022-08-12 20:03:49 -0300

    Ejercicio 7

---
+++ Ejercicio 7.py
@@ -0,0 +1,9 @@
+Historial3 = (9530, 4120, 4580, 1500, 890, 7516, 426)
+
+promedio = sum(list(Historial3))/len(Historial3) 
+print("El promedio gastado por atención de “Lennon” es:", promedio)
+
+if promedio > 4500:
+    print("Se ha excedido del gasto promedio para su mascota")
+else:
+    print("")
